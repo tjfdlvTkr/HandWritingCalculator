76cef6d2a75f6cf47669e547250a987b853efbc3
Junha Shin <junha212@gmail.com>
2022-12-11 02:57:13 +0900

    Add main.py

---
+++ main.py
@@ -0,0 +1,116 @@
+import cv2
+import datetime
+from PIL import ImageFont, ImageDraw, Image
+import numpy as np
+import sys
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+
+class ShowVideo(QtCore.QObject):
+
+    flag = 0
+
+    camera = cv2.VideoCapture(0)
+
+    ret, image = camera.read()
+    height, width = image.shape[:2]
+
+    VideoSignal1 = QtCore.pyqtSignal(QtGui.QImage)
+    VideoSignal2 = QtCore.pyqtSignal(QtGui.QImage)
+
+    def __init__(self, parent=None):
+        super(ShowVideo, self).__init__(parent)
+
+    @QtCore.pyqtSlot()
+    def startVideo(self):
+        global image
+
+        self.run_video = True
+        while self.run_video:
+            ret, image = self.camera.read()
+            color_swapped_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            self.qt_image = QtGui.QImage(color_swapped_image.data,
+                                    self.width,
+                                    self.height,
+                                    color_swapped_image.strides[0],
+                                    QtGui.QImage.Format_RGB888)
+            self.VideoSignal1.emit(self.qt_image)
+
+            loop = QtCore.QEventLoop()
+            QtCore.QTimer.singleShot(25, loop.quit) #25 ms
+            loop.exec_()
+
+    @QtCore.pyqtSlot()
+    def savePicture(self):
+        filename = './capture.png'
+        self.qt_image.save(filename)
+        
+class ImageViewer(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(ImageViewer, self).__init__(parent)
+        self.image = QtGui.QImage()
+        self.setAttribute(QtCore.Qt.WA_OpaquePaintEvent)
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.drawImage(0, 0, self.image)
+        self.image = QtGui.QImage()
+
+    def initUI(self):
+        self.setWindowTitle('Test')
+
+    @QtCore.pyqtSlot(QtGui. QImage)
+    def setImage(self, image):
+        if image.isNull():
+            print("Viewer Dropped frame!")
+
+        self.image = image
+        if image.size() != self.size():
+            self.setFixedSize(image.size())
+        self.update()
+        
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    
+    thread = QtCore.QThread()
+    thread.start()
+    vid = ShowVideo()
+    vid.moveToThread(thread)
+
+    image_viewer1 = ImageViewer()
+
+    vid.VideoSignal1.connect(image_viewer1.setImage)
+
+    push_button1 = QtWidgets.QPushButton('Start')
+    push_button2 = QtWidgets.QPushButton('Capture')
+    blank = QtWidgets.QLabel(' ')
+    input_sick = QtWidgets.QLabel('식:')
+    result = QtWidgets.QLabel('결과값')
+    input_sick.setAlignment(Qt.AlignCenter)
+    result.setAlignment(Qt.AlignCenter)
+    
+    push_button1.clicked.connect(vid.startVideo)
+    push_button2.clicked.connect(vid.savePicture)
+
+    vertical_layout = QtWidgets.QVBoxLayout()
+    horizontal_layout = QtWidgets.QHBoxLayout()
+    horizontal_layout.addWidget(image_viewer1)
+    vertical_layout.addLayout(horizontal_layout)
+    vertical_layout.addWidget(push_button1)
+    vertical_layout.addWidget(push_button2)
+    vertical_layout.addWidget(blank)
+    vertical_layout.addWidget(input_sick)
+    vertical_layout.addWidget(result)
+
+    layout_widget = QtWidgets.QWidget()
+    layout_widget.setLayout(vertical_layout)
+
+    main_window = QtWidgets.QMainWindow()
+    main_window.setCentralWidget(layout_widget)
+    main_window.show()
+    sys.exit(app.exec_())
